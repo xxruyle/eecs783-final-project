9092b1e343b7d4eb853b53a6ee2a49aeb15f6553
hmhoopes <hmhoopes@gmail.com>
2025-11-30 15:11:13 -0600

    Un-comment-out easy ocr check in text extraction

--- detect.py
+++ detect.py
@@ -2,7 +2,7 @@ from text.vlm_test import run_vlm_test
 from text.easyocr_test import run_easyocr_test
 
 def run_tests():
-  #run_easyocr_test()
+  run_easyocr_test()
   run_vlm_test()
 
 if __name__ == "__main__":
