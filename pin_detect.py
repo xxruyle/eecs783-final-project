cd31e4743fc6c4d29493ef89ce37e45b0464ef63
hmhoopes <hmhoopes@gmail.com>
2025-12-04 00:20:36 -0600

    Update toolflow

--- pin_detect.py
+++ pin_detect.py
@@ -2,7 +2,11 @@ from pin.cnn_detect import run_cnn_img_detect
 from pin.depth_detect_pins import run_depth_detect
 
 def run_img_tests():
-  #run_depth_detect()
+  print
+  
+  print("="*80 + "\n" + f"Depth Based Check")
+  run_depth_detect()
+  print("="*80 + "\n" + f"Neural Net based check")
   run_cnn_img_detect()
 
 
